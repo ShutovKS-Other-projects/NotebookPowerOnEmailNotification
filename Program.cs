954910ebf145f51e7cc5305e8123f00e92c9a557
ShutovKS <i@shutovks.ru>
2023-05-06 00:33:40 +0300

    Created

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using MailKit.Net.Smtp;
+using MimeKit;
+using Windows.Media.Capture;
+using Windows.Storage;
+
+
+/*
+
+var mailMessage = new MimeMessage();
+mailMessage.From.Add(new MailboxAddress("from name", "schutow.kir@yandex.ru"));
+mailMessage.To.Add(new MailboxAddress("to name", "schutow.kir@yandex.ru"));
+mailMessage.Subject = "subject";
+mailMessage.Body = new TextPart("plain")
+{
+    Text = "The computer is on. \n" +
+           $"Time: {DateTime.Now}\n" 
+           // "Place:  \n"
+};
+
+using (var smtpClient = new SmtpClient())  // Sending a message
+{
+    smtpClient.Connect("smtp.yandex.ru", 465, true);
+    smtpClient.Authenticate("schutow.kir", "bfrkntpxhiymjipi");
+    smtpClient.Send(mailMessage);
+    smtpClient.Disconnect(true);
+}
+
+Environment.Exit(0); // program exit
+*/
+
